d7e427671ef2e71f7178fc92dd84dd6802fc1ac8
wupeng01 <wupeng01@bilibili.com>
2020-05-29 16:13:18 +0800

    fix

--- assets/js/2.a064b6fafda6674ac969.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1:function(n,o,w){},9:function(n,o,w){"use strict";w.r(o);w(1)}},[[9,0]]]);
-//# sourceMappingURL=2.a064b6fafda6674ac969.js.map
